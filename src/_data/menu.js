d2162823c8ce1138443aefe9f9445059f3a31c9f
alan191006 <hdmquan@outlook.com>
2024-08-02 14:31:37 +1000

    refactor: Edit content and hero section
    - Add unedited contents to several section
    - Put spaceholder text elsewhere
    - Change hero section image and divider

---
+++ src/_data/menu.js
@@ -0,0 +1,82 @@
+module.exports = {
+    title: "Our Menu",
+    sections: [
+      {
+        category: "one",
+        name: "Pizza",
+        items: [
+          {
+            name: "Purple Corn Breakfast",
+            price: "$56",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.",
+            image: "https://csimg.nyc3.cdn.digitaloceanspaces.com/Food-Menu/food2.jpg"
+          },
+          {
+            name: "Purple Corn Tostada",
+            price: "$56",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.",
+            image: "https://csimg.nyc3.cdn.digitaloceanspaces.com/Food-Menu/food2.jpg"
+          }
+          // Add more items as needed
+        ]
+      },
+      {
+        category: "two",
+        name: "Bánh ngọt",
+        items: [
+          {
+            name: "Purple Corn Lunch",
+            price: "$56",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.",
+            image: "https://csimg.nyc3.cdn.digitaloceanspaces.com/Food-Menu/food2.jpg"
+          },
+          {
+            name: "Purple Corn Tostada",
+            price: "$56",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.",
+            image: "https://csimg.nyc3.cdn.digitaloceanspaces.com/Food-Menu/food2.jpg"
+          }
+          // Add more items as needed
+        ]
+      },
+      {
+        category: "three",
+        name: "Bánh mặn",
+        items: [
+          {
+            name: "Purple Corn Lunch",
+            price: "$56",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.",
+            image: "https://csimg.nyc3.cdn.digitaloceanspaces.com/Food-Menu/food2.jpg"
+          },
+          {
+            name: "Purple Corn Tostada",
+            price: "$56",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.",
+            image: "https://csimg.nyc3.cdn.digitaloceanspaces.com/Food-Menu/food2.jpg"
+          }
+          // Add more items as needed
+        ]
+      },
+      {
+        category: "four",
+        name: "Khác",
+        items: [
+          {
+            name: "Purple Corn Lunch",
+            price: "$56",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.",
+            image: "https://csimg.nyc3.cdn.digitaloceanspaces.com/Food-Menu/food2.jpg"
+          },
+          {
+            name: "Purple Corn Tostada",
+            price: "$56",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.",
+            image: "https://csimg.nyc3.cdn.digitaloceanspaces.com/Food-Menu/food2.jpg"
+          }
+          // Add more items as needed
+        ]
+      }
+    ]
+  };
+  
---
+++ src/_data/team.js
@@ -0,0 +1,20 @@
+module.exports = {
+    title: "Gia đình nhà Nini",
+    description: "Spaceholder",
+    members: [
+      {
+        name: "Esther Howard",
+        job: "Project Manager",
+        imageMobile: "https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/non-profit-team-one.png",
+        imageDesktop: "https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/non-profit-team-one.png",
+        alt: "volunteer",
+      },
+      {
+        name: "Jane Doe",
+        job: "Developer",
+        imageMobile: "https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/non-profit-team-two.png",
+        imageDesktop: "https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/non-profit-team-two.png",
+        alt: "developer",
+      },
+    ]
+};
